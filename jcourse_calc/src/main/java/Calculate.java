71c7d00d0b9e8847902f0a306ce015d469f87818
TkachenkoAlex <a.tkachenko@bftcom.com>
2016-09-22 13:23:36 +0300

    add sum

--- jcourse_calc/src/main/java/Calculate.java
+++ jcourse_calc/src/main/java/Calculate.java
@@ -3,7 +3,10 @@ public class Calculate {
 
     public static void main(String[] args) {
         System.out.println("Ololo111");
-
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int sum = first + second;
+        System.out.println("sum = " + sum);
     }
 
 }
